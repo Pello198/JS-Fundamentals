31ffcc955ca69e5847f9503ee2852cea00f4d902
pello198 <kwemoipello@gmail.com>
2025-10-30 17:12:00 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,9 +2,7 @@
 
 const occurrences = parseInt(process.argv[2]);
 
-if (isNaN(occurrences) || occurrences <= 0) {
-  console.log("Missing number of occurrences");
-} else {
+if (!isNaN(occurrences) && occurrences > 0) {
   for (let i = 0; i < occurrences; i++) {
     console.log("C is fun");
   }
